99d8de593af2e1a2ec4d22c46870cd4ca4716cd5
Querela <ek.92@arcor.de>
2013-05-18 13:32:20 +0200

    Script to download all URLs.

---
+++ scripts/load_batch.sh
@@ -0,0 +1,90 @@
+#!/bin/bash
+
+#
+# load_batch.sh
+# Batch file to get all urls in the batch file from IA 2013
+#
+
+batchFile=$1
+
+###############################################################################
+
+# Checking input batch file
+if [ ${batchFile##*.} != "zip" ]; then
+        echo "ERROR: No Zip-File found."
+        exit 1
+fi
+
+# Create a fresh working directory
+workDir="$(dirname $batchFile)/${batchFile%.*}"
+
+if [ -e "$workDir" ]; then
+	read -r -p "Remove \"$workDir\". Are you sure? [Y/n]} " response
+	#if [ "$response" = "Y" ]; then
+	if [[ $response =~ ^([yY][eE][sS]|[yY])$ ]]; then
+		rm -r "$workDir"
+	fi
+fi
+
+mkdir -p "$workDir"
+
+###############################################################################
+
+tempFileNr=0
+
+# Iterate over all entries in the batch file
+for file in `unzip -qq -l "$batchFile" | awk '{print $4}'`; do
+	((tempFileNr += 1))
+
+	echo "----------"
+	echo "Working on $file:"
+
+	#targetFile="$workDir/$tempFileNr/tempfile_$tempFileNr"
+
+	# Unzip single entry into $workDir
+	unzip -q "$batchFile" "$file" -d "$workDir"
+
+	# Files to work on
+	inFile="$workDir/$file"
+	outFile="$workDir/${file}.out"
+
+	# Replace ; with tabs for awk tool
+	sed -e "s/;/\t/g" "$inFile" > "$outFile" 
+
+	outDir="${inFile%.*}"
+	mkdir "$outDir"
+
+	#######################################################################
+
+	while read line
+	do
+		((lineNr += 1))
+		url=`echo "$line" | awk '{print $1}'`
+		categ=`echo "$line" | awk '{print $2}'`
+		#echo "Processing $url with category $categ."
+
+		urlFile="$outDir/page_$lineNr.htm"
+		headerFile="$outDir/header_$lineNr.txt"
+
+		# Get document with cURL
+		# Options: --compressed, --create-dirs, --fail ?
+		curl --dump-header "$headerFile" --output "$urlFile" --retry 3 --location --write-out "Got \"%{url_effective}\" -> \"$urlFile\" (%{size_download} Bytes) - STATUS: [%{http_code}]\n" -# "$url"
+
+		# Get response code
+		ok=`cat "$headerFile" | grep -e "HTTP/1.1 200 OK"`
+		if [ "$ok" = "" ]; then
+			echo "ERROR in $tempFileNr-$lineNr ?"
+			> "$urlFile.fail"
+			continue
+		fi
+
+		# Get keywords
+		keywordFile="$outDir/keywords_$lineNr.txt"
+		#cat "$urlFile" | grep "keywords" > "$keywordFile"
+
+	done < "$outFile"
+
+	#######################################################################
+
+
+done
